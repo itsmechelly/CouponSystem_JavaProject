6d7a610c391f72670ca1d748e6532a308699dfac
hllyl <hlly.loc@gmail.com>
2021-01-14 17:30:44 +0200

    added beans package

---
+++ CouponSystem_JavaProject/src/com/coupon_system_beans/Category.java
@@ -0,0 +1,9 @@
+package com.coupon_system_beans;
+
+public enum Category {
+
+	FOOD,
+	ELECTRICITY,
+	RESTAURANT,
+	VACATION
+}
---
+++ CouponSystem_JavaProject/src/com/coupon_system_beans/CustomerVsCoupons.java
@@ -0,0 +1,32 @@
+package com.coupon_system_beans;
+
+public class CustomerVsCoupons {
+
+	private int customerId;
+	private int couponId;
+
+	public CustomerVsCoupons() {
+		super();
+	}
+
+	public int getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+
+	public int getCouponId() {
+		return couponId;
+	}
+
+	public void setCouponId(int couponId) {
+		this.couponId = couponId;
+	}
+
+	@Override
+	public String toString() {
+		return "CustomerVsCoupons [customerId=" + customerId + ", couponId=" + couponId + "]";
+	}
+}
