a3c97e6fa3b03ff974e84f676eff279a785dc58b
hllyl <hlly.lov@gmail.com>
2021-01-27 16:18:25 +0200

    Finished ClientFacade class

--- CouponSystem_JavaProject/src/com/couponsystem/facade/ClientFacade.java
+++ CouponSystem_JavaProject/src/com/couponsystem/facade/ClientFacade.java
@@ -1,5 +1,24 @@
 package com.couponsystem.facade;
 
+import com.couponsystem.dao.CompaniesDAO;
+import com.couponsystem.dao.CouponsDAO;
+import com.couponsystem.dao.CustomersDAO;
+import com.couponsystem.dbdao.CompaniesDBDAO;
+import com.couponsystem.dbdao.CouponsDBDAO;
+import com.couponsystem.dbdao.CustomersDBDAO;
+import com.couponsystem.exceptions.LogException;
+
 public abstract class ClientFacade {
 
+	protected CompaniesDAO companiesDAO = null;
+	protected CustomersDAO customersDAO = null;
+	protected CouponsDAO couponsDAO = null;
+
+	public ClientFacade() {
+		companiesDAO = new CompaniesDBDAO();
+		customersDAO = new CustomersDBDAO();
+		couponsDAO = new CouponsDBDAO();
+	}
+
+	public abstract boolean login(String email, String password) throws LogException;
 }
