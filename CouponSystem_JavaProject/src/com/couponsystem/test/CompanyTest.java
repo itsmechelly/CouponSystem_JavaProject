d2e2c77d2d6b558fdeb110a200c2350698ae7edd
hllyl <hlly.lov@gmail.com>
2021-01-31 15:01:15 +0200

    CompanyTest class prep

--- CouponSystem_JavaProject/src/com/couponsystem/test/CompanyTest.java
+++ CouponSystem_JavaProject/src/com/couponsystem/test/CompanyTest.java
@@ -1,5 +1,34 @@
 package com.couponsystem.test;
 
+import com.couponsystem.dao.CategoryDAO;
+import com.couponsystem.dao.CompaniesDAO;
+import com.couponsystem.dao.CouponsDAO;
+import com.couponsystem.dao.CustomersDAO;
+import com.couponsystem.dbdao.CategoryDBDAO;
+import com.couponsystem.dbdao.CompaniesDBDAO;
+import com.couponsystem.dbdao.CouponsDBDAO;
+import com.couponsystem.dbdao.CustomersDBDAO;
+import com.couponsystem.facade.CompanyFacade;
+import com.couponsystem.utils.TestUtils;
+
 public class CompanyTest {
 
+	public static void companyTest() {
+
+		CompanyFacade companyFacade = new CompanyFacade();
+
+		CompaniesDAO companiesDAO = new CompaniesDBDAO();
+		CustomersDAO customersDAO = new CustomersDBDAO();
+		CouponsDAO couponsDAO = new CouponsDBDAO();
+		CategoryDAO categoryDAO = new CategoryDBDAO();
+
+		TestUtils.DoubleSeparatedLine();
+		TestUtils.companyFacade();
+
+		try {
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
 }
