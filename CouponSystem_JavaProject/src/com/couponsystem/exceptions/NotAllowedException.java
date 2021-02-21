5913c0142cb647b28c067c4229c4375f4b846ead
hllyl <hlly.lov@gmail.com>
2021-02-21 18:39:04 +0200

    ...

---
+++ CouponSystem_JavaProject/src/com/couponsystem/exceptions/NotAllowedException.java
@@ -0,0 +1,15 @@
+package com.couponsystem.exceptions;
+
+public class NotAllowedException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public NotAllowedException(String string) {
+		super("System error, unable to " + string);
+
+	}
+
+}
