bc57733af830e1002f3b9603d8dd7eb90775155a
hllyl <hlly.lov@gmail.com>
2021-02-21 18:40:40 +0200

    ...

--- CouponSystem_JavaProject/src/com/couponsystem/facade/AdminFacade.java
+++ CouponSystem_JavaProject/src/com/couponsystem/facade/AdminFacade.java
@@ -3,8 +3,11 @@ package com.couponsystem.facade;
 import java.util.List;
 
 import com.couponsystem.beans.Company;
+import com.couponsystem.beans.Coupon;
+import com.couponsystem.beans.Customer;
 import com.couponsystem.exceptions.AlreadyExistException;
 import com.couponsystem.exceptions.LogException;
+import com.couponsystem.exceptions.NotAllowedException;
 
 public class AdminFacade extends ClientFacade {
 
@@ -19,7 +22,7 @@ public class AdminFacade extends ClientFacade {
 	}
 
 //	------------------------------------------------------------------------------------------------------------
-	
+
 	public void addCompany(Company company) throws AlreadyExistException {
 
 		List<Company> companies = companiesDAO.getAllCompanies();
@@ -34,4 +37,95 @@ public class AdminFacade extends ClientFacade {
 		}
 		companiesDAO.addCompany(company);
 	}
+
+	public void updateCompany(int companyID, Company company) throws NotAllowedException, AlreadyExistException {
+
+		Company comp = companiesDAO.getOneCompany(companyID);
+
+		if (comp.getId() != company.getId()) {
+			throw new NotAllowedException("update company id number.");
+		}
+
+		if (!comp.getName().equals(company.getName())) {
+			throw new NotAllowedException("update company name.");
+		}
+
+		companiesDAO.updateCompany(company);
+	}
+
+	public void deleteCompany(int companyID) {
+
+		List<Coupon> existingCouponsFromDB = couponsDAO.getAllCoupons();
+
+		for (Coupon coupon : existingCouponsFromDB) {
+			if (coupon.getCompanyId() == companyID) {
+				couponsDAO.deleteCouponPurchaseForFacade(coupon.getId());
+				couponsDAO.deleteCoupon(coupon.getId());
+			}
+		}
+		companiesDAO.deleteCompany(companyID);
+	}
+
+	public Company getOneCompany(int companyID) {
+		return companiesDAO.getOneCompany(companyID);
+	}
+
+	public List<Company> getAllCompanies() {
+
+		List<Company> comp = companiesDAO.getAllCompanies();
+		for (Company c : comp) {
+			c.setCoupons(couponsDAO.getAllCouponsByCompanyID(c.getId()));
+		}
+		return comp;
+	}
+
+//	------------------------------------------------------------------------------------------------------------
+
+	public void addCustomer(Customer customer) throws AlreadyExistException {
+
+		List<Customer> customers = customersDAO.getAllCustomers();
+
+		for (Customer cust : customers) {
+			if (cust.getEmail().equalsIgnoreCase(customer.getEmail())) {
+				throw new AlreadyExistException("Customer email ", customer.getEmail());
+			}
+		}
+		customersDAO.addCustomer(customer);
+	}
+
+	public void updateCustomer(int customerID, Customer customer) throws NotAllowedException {
+
+		Customer cust = customersDAO.getOneCustomer(customerID);
+
+		if (cust.getId() != customer.getId()) {
+			throw new NotAllowedException("update customer id number.");
+		}
+
+		customersDAO.updateCustomer(customerID, customer);
+	}
+
+	public void deleteCustomer(int customerID) {
+
+		List<Coupon> originalCoupons = couponsDAO.getAllCoupons();
+
+		for (Coupon coup : originalCoupons) {
+			couponsDAO.deleteCouponPurchase(customerID, coup.getId());
+		}
+
+		customersDAO.deleteCustomer(customerID);
+	}
+
+	public Customer getOneCustomer(int customerID) {
+		return customersDAO.getOneCustomer(customerID);
+	}
+
+	public List<Customer> getAllCustomers() {
+
+		List<Customer> cust = customersDAO.getAllCustomers();
+		for (Customer c : cust) {
+			c.setCoupons(couponsDAO.getAllCouponsByCustomerID(c.getId()));
+		}
+		return cust;
+	}
+
 }
