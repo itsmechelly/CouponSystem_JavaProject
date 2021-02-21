2c1a06e60d1b18add80704a012d8e7904efb79e1
hllyl <hlly.lov@gmail.com>
2021-02-21 18:44:11 +0200

    ...

---
+++ CouponSystem_JavaProject/src/com/couponsystem/exceptions/PurchaseCouponException.java
@@ -0,0 +1,14 @@
+package com.couponsystem.exceptions;
+
+public class PurchaseCouponException extends Exception {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public PurchaseCouponException(String string) {
+		super(string);
+
+	}
+}
