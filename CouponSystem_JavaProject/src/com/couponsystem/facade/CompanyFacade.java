1db920cb848895a5c105e2832466bf26299438f8
hllyl <hlly.lov@gmail.com>
2021-02-21 18:52:12 +0200

    ...

--- CouponSystem_JavaProject/src/com/couponsystem/facade/CompanyFacade.java
+++ CouponSystem_JavaProject/src/com/couponsystem/facade/CompanyFacade.java
@@ -1,6 +1,15 @@
 package com.couponsystem.facade;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.couponsystem.beans.Category;
+import com.couponsystem.beans.Company;
+import com.couponsystem.beans.Coupon;
+import com.couponsystem.exceptions.AlreadyExistException;
 import com.couponsystem.exceptions.LogException;
+import com.couponsystem.exceptions.NotAllowedException;
+import com.couponsystem.exceptions.PurchaseCouponException;
 
 public class CompanyFacade extends ClientFacade {
 
@@ -29,5 +38,91 @@ public class CompanyFacade extends ClientFacade {
 	}
 
 //	------------------------------------------------------------------------------------------------------------
+
+	public void addCompanyCoupon(Coupon coupon) throws AlreadyExistException {
+
+		List<Coupon> coup = couponsDAO.getAllCouponsByCompanyID(companyId);
+
+		for (Coupon coupons : coup) {
+			if (coupon.getTitle().equals(coupons.getTitle())) {
+				throw new AlreadyExistException("Coupon name: ", coupon.getTitle());
+			}
+		}
+		couponsDAO.addCoupon(coupon);
+	}
+
+	public void updateCompanyCoupon(Coupon coupon) throws NotAllowedException, PurchaseCouponException {
+
+		Coupon coup = couponsDAO.getOneCoupon(coupon.getId());
+
+		if (coup.getId() != coupon.getId()) {
+			throw new NotAllowedException("update id number.");
+		}
+
+		if (coup.getCompanyId() != coupon.getCompanyId()) {
+			throw new NotAllowedException("update id number.");
+		}
+
+		couponsDAO.updateCoupon(coupon);
+	}
+
+	public void deleteCompanyCoupons(int couponId) throws NotAllowedException {
+
+		List<Coupon> coup = couponsDAO.getAllCoupons();
+
+		Coupon couponFromDb = couponsDAO.getOneCoupon(couponId);
+
+		for (Coupon c : coup) {
+
+			if (couponFromDb.getCompanyId() != companyId) {
+				throw new NotAllowedException("delete coupon - not exist, please try again.");
+			}
+
+			if (couponId != c.getId()) {
+//				couponsDAO.deleteCouponPurchaseForFacade(couponId);
+//				couponsDAO.deleteCouponPurchase(this.companyID, couponId);
+				couponsDAO.deleteCoupon(couponId);
+			}
+		}
+	}
+
+	public List<Coupon> getAllCompanyCoupons() {
+
+		return couponsDAO.getAllCouponsByCompanyID(companyId);
+	}
+
+	public List<Coupon> getCompanyCouponsByCategory(Category category) {
+
+		List<Coupon> coup = couponsDAO.getAllCouponsByCompanyID(companyId);
+		List<Coupon> coupByCategory = new ArrayList<Coupon>();
+
+		for (Coupon c : coup) {
+			if (c.getCategory().equals(category)) {
+				coupByCategory.add(c);
+			}
+		}
+		return coupByCategory;
+	}
+
+	public List<Coupon> getCompanyCouponsUnderMaxPrice(double maxPrice) {
+
+		List<Coupon> coup = couponsDAO.getAllCouponsByCompanyID(companyId);
+		List<Coupon> CouponsUnderMaxPrice = new ArrayList<Coupon>();
+
+		for (Coupon c : coup) {
+			if (c.getPrice() < maxPrice) {
+				CouponsUnderMaxPrice.add(c);
+			}
+		}
+		return CouponsUnderMaxPrice;
+	}
+
+	public Company getCompanyDetails() {
+
+		Company comp = companiesDAO.getOneCompany(companyId);
+		comp.setCoupons(getAllCompanyCoupons());
+
+		return comp;
+	}
 
 }
